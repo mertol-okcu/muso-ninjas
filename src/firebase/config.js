aedc887be7278bbc48bc544f8738ed5d56283f98
mertol-okcu <mertolokcu@gmail.com>
2021-01-16 19:46:35 +0300

    setup firebase: firestore, auth, hosting

---
+++ src/firebase/config.js
@@ -0,0 +1,24 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBrMF8vGIoHMReULZ3J31TXa4pcsakpEgk",
+  authDomain: "muso-ninjas-d386f.firebaseapp.com",
+  projectId: "muso-ninjas-d386f",
+  storageBucket: "muso-ninjas-d386f.appspot.com",
+  messagingSenderId: "999597677531",
+  appId: "1:999597677531:web:4198538df7b19c99f425fe",
+};
+
+// init firebase
+firebase.initializeApp(firebaseConfig);
+
+// init services
+const projectFirestore = firebase.firestore();
+const projectAuth = firebase.auth();
+
+// timestamp
+const timestamp = firebase.firestore.FieldValue.serverTimestamp;
+
+export { projectFirestore, projectAuth, timestamp };
